7b3974b5d2b3aa25c50d2e9c7cfa3e305d195306
JIAN JIAN <ukgang@hotmail.com>
2020-07-14 22:05:39 -0400

    1. Bind cache to base job 2. Define logger in base job

--- ibranch/scraping_scheduler/engine/job/Base.py
+++ ibranch/scraping_scheduler/engine/job/Base.py
@@ -1,17 +1,28 @@
+import logging
 from abc import ABC
+from queue import Queue
 
 from ibranch.scraping_scheduler.configuration.Configurator import Configuration
+from ibranch.scraping_scheduler.domain.System import Cache
 
 
 class BaseJob(ABC):
     def __init__(self, cache_name=None):
+        self._logger = logging.getLogger(type(self).__name__)
         # Data queue
         # key = domain, value = url list
         if cache_name:
             self._cache_name = cache_name
+            cache_catalog = Cache().get_new_cache(Queue)
+            Cache().register_catelog(self.cache_name, cache_catalog)
+            self._cache = cache_catalog
 
     def run(self):
         raise NotImplementedError()
+
+    @property
+    def logger(self):
+        return self._logger
 
     @property
     def cache_name(self):
