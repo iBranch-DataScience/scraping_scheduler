721b237f71c43a7f4922f19dcc9c15de7a19b4e5
JIAN JIAN <ukgang@hotmail.com>
2020-07-14 22:30:30 -0400

    Fix cache initialization bug

--- ibranch/scraping_scheduler/engine/job/Base.py
+++ ibranch/scraping_scheduler/engine/job/Base.py
@@ -1,4 +1,5 @@
 import logging
+import threading
 from abc import ABC
 from queue import Queue
 
@@ -7,13 +8,23 @@ from ibranch.scraping_scheduler.domain.System import Cache
 
 
 class BaseJob(ABC):
+    mutex = threading.Lock()
+
     def __init__(self, cache_name=None):
         self._logger = logging.getLogger(type(self).__name__)
         # Data queue
         # key = domain, value = url list
         if cache_name:
             self._cache_name = cache_name
-            cache_catalog = Cache().get_new_cache(Queue)
+
+            try:
+                BaseJob.mutex.acquire(blocking=True)
+                if Cache().cache_exists(self.cache_name):
+                    cache_catalog = Cache().get_existing_cache(self.cache_name)
+                else:
+                    cache_catalog = Cache().get_new_cache(Queue)
+            finally:
+                BaseJob.mutex.release()
             Cache().register_catelog(self.cache_name, cache_catalog)
             self._cache = cache_catalog
 
